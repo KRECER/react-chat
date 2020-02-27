96ecf55280e23cd190cda971898facedd3a4e614
Igor Sudenko <sudenkoweb@gmail.com>
2020-02-27 06:48:27 +0200

    Добавил функцию для извлечения инициалов со строки

---
+++ src/utils/title-initials.js
@@ -0,0 +1,8 @@
+export const titleInitials = (title) => {
+  try {
+    return title.split(' ').map(word => word[0]).map(chat => chat.toUpperCase()).slice(0, 2).join('');
+  } catch(e) {
+    console.error(e);
+    return '';
+  }
+};
